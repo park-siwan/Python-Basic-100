82a420876c03d7a68e06349db8db5c2ad1397baf
park-siwan <pswan3168@gmail.com>
2021-04-12 13:53:12 +0900

    TIL : python basic - output

---
+++ 1. 출력/출력.py
@@ -0,0 +1,20 @@
+print('Hello')
+
+print('Hello World')
+
+print('''\
+Hello
+world
+''')
+
+print("'Hello World'")
+
+print('"!@#$%^&*()')
+
+print('"C:\Download\hello.cpp"')
+
+print('''\
+┌┬┐
+├┼┤
+└┴┘
+''')
